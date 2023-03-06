c9301bdf5937fb86439aeb184ba8ee3a3cc29dbc
guilhermeportela <38728633+guischpor@users.noreply.github.com>
2023-03-06 18:18:36 -0300

    up

--- bin/server_handler.dart
+++ bin/server_handler.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
@@ -6,7 +8,11 @@ class ServerHandleR {
     final router = Router();
 
     router.get('/', (Request request) async {
-      return Response(200, body: 'Primeira Rota!');
+      return Response(
+        200,
+        body: '<h1>Primeira Rota!</h1>',
+        headers: {'content-type': 'text/html'},
+      );
     });
 
     //http://localhost:8080/ola/mundo
@@ -26,6 +32,33 @@ class ServerHandleR {
       String? nome = req.url.queryParameters['nome'];
       String? idade = req.url.queryParameters['idade'];
       return Response.ok('Query params: $nome, $idade anos');
+    });
+
+    router.post('/login', (Request req) async {
+      String result = await req.readAsString();
+      Map json = jsonDecode(result);
+      var usuario = json['usuario'];
+      var senha = json['senha'];
+
+      //convertendo e retornando um json
+      Map<String, dynamic> responseReturn = ({
+        "token": "token123",
+        "user_id": 1,
+        "user_name": usuario,
+      });
+
+      String jsonResponse = jsonEncode(responseReturn);
+
+      //se usuario == admin@admin.com e senha == 123456
+      if (usuario == 'admin@admin.com' && senha == '123456') {
+        return Response.ok(
+          jsonResponse,
+          headers: {'content-type': 'application/json'},
+        );
+      } else {
+        //se não
+        return Response.forbidden('Acesso negado! Usuário ou senha inválidos!');
+      }
     });
 
     return router;
